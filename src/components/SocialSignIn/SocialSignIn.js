3cfbe7d232e496ac466a7691698dda2fdbe4ccc3
Shahadat Hossain <shahadathossain4536@gmail.com>
2022-04-17 11:08:55 +0600

    google singin & github singin add

--- src/components/SocialSignIn/SocialSignIn.js
+++ src/components/SocialSignIn/SocialSignIn.js
@@ -1,21 +1,30 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import google from '../../image/social login/google.png'
 import facebook from '../../image/social login/facebook.png'
 import github from '../../image/social login/github.png'
-
+import auth from '../../firebase.init';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 
 const SocialSignIn = () => {
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithGithub, githubUser, githubLoading, githubError] = useSignInWithGithub(auth);
+
+    // let errorElement;
+
+    // console.log(googleError, githubError);
+    // if (googleError || githubError) {
+    //     errorElement = <p>Error:{googleError?.githubError}</p>;
+    // }
     return (
         <div className='d-flex justify-content-end'>
-            <button className='btn btn-link'>
+            <button onClick={() => signInWithGoogle()} className='btn btn-link'>
                 <img className='' src={google} width={'20px'} alt="" srcset="" />
             </button>
             <button className='btn btn-link'>
                 <img className='' src={facebook} width={'20px'} alt="" srcset="" />
             </button>
-            <button className='btn btn-link'>
+            <button onClick={() => signInWithGithub()} className='btn btn-link'>
                 <img className='' src={github} width={'20px'} alt="" srcset="" />
             </button>
         </div>
